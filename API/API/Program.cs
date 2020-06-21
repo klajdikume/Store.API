8969984feef1d801b8e1f105f9999687ffe8f8a1
klajdikume <klajdi.kume@fshnstudent.info>
2020-06-21 17:43:48 +0200

    c

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -5,17 +5,42 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
     public class Program
     {
         //called when started the project
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             //configure host 
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            //access to data context
+            //don't have control to lifetime
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                //log any info 
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync(); //apply pending migration to db or create db if not exists
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
+                }
+                catch(Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+
+            host.Run();
         }
         //configure defaults for app
         //configure loggin //read config files
