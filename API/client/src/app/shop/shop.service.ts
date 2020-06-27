3bd3da95a87f24426b0dd728a0306054e5a31126
klajdikume <klajdi.kume@fshnstudent.info>
2020-06-27 13:01:56 +0200

    routers added

--- API/client/src/app/shop/shop.service.ts
+++ API/client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IBrand } from '../shared/models/brand';
 import { IType } from '../shared/models/productType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root' // where this is provided in
@@ -44,6 +45,10 @@ export class ShopService {
           return response.body; // IPagination object
         })
       );
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
