9aae8ee5af43fffe35671cd6de670fa7869e1ccb
klajdikume <klajdi.kume@fshnstudent.info>
2020-06-28 12:13:47 +0200

    checkout and orders

--- API/client/src/app/shop/product-item/product-item.component.ts
+++ API/client/src/app/shop/product-item/product-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/product';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-product-item',
@@ -10,9 +11,13 @@ export class ProductItemComponent implements OnInit {
 
   @Input() product: IProduct; // receive smth from parent component with Input prop
 
-  constructor() { }
+  constructor(private basketService: BasketService) { }
 
   ngOnInit() {
+  }
+
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product);
   }
 
 }
